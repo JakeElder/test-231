d213e2b7faaef53af519f0f93161bfd5a657c8a4
Jake Elder <jake@jakes.world>
2020-04-20 21:06:24 +0700

    Add Helmet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import 'reset-css'
+import { Helmet } from 'react-helmet'
 
 import GlobalStyles from '../GlobalStyles'
+
+import useTitle from '../../hooks/use-title'
 
 import './App.css'
 
 const App = ({ children }) => {
-  return <GlobalStyles>{children}</GlobalStyles>
+  const title = useTitle()
+  return (
+    <GlobalStyles>
+      <Helmet>
+        <meta name="robots" content="noindex" />
+        <title>{title}</title>
+      </Helmet>
+      {children}
+    </GlobalStyles>
+  )
 }
 
 export default App
