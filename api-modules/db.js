02c106aa4e54bbd042a416061046b202cc40a0c9
Jake Elder <jake@jakes.world>
2020-04-21 13:29:25 +0700

    Seed utils

--- api-modules/db.js
+++ api-modules/db.js
@@ -1,26 +1,58 @@
 const { MongoClient } = require('mongodb')
 const url = require('url')
+const short = require('short-uuid')
+const uuid = short('abcdefghijklmnopqrstuvwxyz0123456789-')
 
 let cachedDb = null
+let cachedClient = null
 
 async function connectToDatabase(uri) {
   if (cachedDb) {
     return cachedDb
   }
 
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   const db = await client.db(url.parse(uri).pathname.substr(1))
 
   cachedDb = db
+  cachedClient = client
+
   return db
 }
 
-async function addSession({ user }) {
+async function addSession({ session }) {}
 
+async function reset() {
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+  const collection = await db.collection('sessions')
+  await collection.deleteMany({})
+  await cachedClient.close()
+}
+
+function prepareSession(session) {
+  return {
+    name: session.name,
+    id: session.id || uuid.new(),
+    answers: session.answers || []
+  }
+}
+
+async function seed() {
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+  const collection = await db.collection('sessions')
+  const sessions = require('../seed.json').map(prepareSession)
+  await collection.insertMany(sessions)
+  await cachedClient.close()
 }
 
 module.exports = {
   connect: connectToDatabase,
   connectToDatabase,
-  addSession
+  addSession,
+  reset,
+  seed,
+  prepareSession
 }
