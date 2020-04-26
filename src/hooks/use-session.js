beaa3401c80caee4208480d8f6dab2bde9af7e93
Jake Elder <jake@jakes.world>
2020-04-26 18:05:44 +0700

    Make timer tick

--- src/hooks/use-session.js
+++ src/hooks/use-session.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import globalHook from './use-global-hook'
 
-const initialState = { session: null }
+const initialState = { session: null, intervalId: null }
 const actions = {
-  set(store, value) {
-    store.setState({ session: value })
+  set(store, session) {
+    let intervalId = null
+
+    if (store.state.intervalId) {
+      clearInterval(store.state.intervalId)
+    }
+
+    if (
+      session !== null &&
+      session.commenced !== null &&
+      session.completed === null &&
+      session.timePassed < session.timeAllocated
+    ) {
+      intervalId = setInterval(() => {
+        store.actions.tick()
+      }, 1000)
+    }
+
+    store.setState({ session, intervalId })
+  },
+  tick(store) {
+    const { timePassed, timeAllocated } = store.state.session
+    const newTimePassed = Math.min(timePassed + 1000, timeAllocated)
+    if (newTimePassed === timeAllocated) {
+      clearInterval(store.state.intervalId)
+    }
+    store.setState({
+      session: {
+        ...store.state.session,
+        timePassed: newTimePassed
+      }
+    })
   }
 }
 
