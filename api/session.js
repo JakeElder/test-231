2dec6d064758fc7636cd4ee1232f5c5db5fd8f43
Jake Elder <jake@jakes.world>
2020-04-30 16:48:19 +0700

    Add [GET] /api/session end point

--- api/session.js
+++ api/session.js
@@ -1,13 +1,21 @@
 const { connect, uuid } = require('../api-modules/db')
+const { normalizeSession } = require('./session/[id]')
 
 module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
+  if (req.method !== 'POST' && req.method !== 'GET') {
     res.status(404).send()
     return
   }
 
   const db = await connect(process.env.MONGODB_URI)
   const collection = await db.collection('sessions')
+
+  if (req.method === 'GET') {
+    const cursor = await collection.find({})
+    const data = await cursor.toArray()
+    res.status(200).json({ data: data.map(normalizeSession) })
+    return
+  }
 
   const session = {
     id: uuid(),
