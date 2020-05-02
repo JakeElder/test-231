86482b382ebb71ad530a457d75a9e7f3077b8523
Jake Elder <jake@jakes.world>
2020-05-02 10:14:34 +0700

    Add AudioPlayer and Response to ReviewPage

--- src/stories/ReviewPage.stories.js
+++ src/stories/ReviewPage.stories.js
@@ -5,6 +5,8 @@ import { PureReviewPage } from '../layouts/ReviewPage'
 import { Default as Header } from './TestPartHeader.stories'
 import { Default as SectionHeader } from './ReviewSectionHeader.stories'
 import { Default as Instruction } from './Instruction.stories'
+import { Default as AudioPlayer } from './AudioPlayer.stories'
+import { Default as Response } from './DemarkedCopy.stories'
 
 export default {
   title: 'Layout :: Review Page',
@@ -23,6 +25,12 @@ export const Default = () => (
       <PureReviewPage.Instruction>
         <Instruction />
       </PureReviewPage.Instruction>
+      <PureReviewPage.AudioPlayer>
+        <AudioPlayer />
+      </PureReviewPage.AudioPlayer>
+      <PureReviewPage.Response>
+        <Response />
+      </PureReviewPage.Response>
     </PureReviewPage.Section>
   </PureReviewPage>
 )
