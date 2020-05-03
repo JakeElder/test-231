4027a171c365d6a2a458dae1f71442f37937a3aa
Jake Elder <jake@jakes.world>
2020-05-03 10:28:03 +0700

    Complete Review Page Section 1 Part 1

--- src/pages/review.js
+++ src/pages/review.js
@@ -1,20 +1,26 @@
+// Vendor
 import 'reset-css'
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
+// Components
 import GlobalStyles from '../components/GlobalStyles'
 import { PureReviewPage as Review } from '../layouts/ReviewPage'
-import { Question as Section1Part1Question, Instruction as Section1Part1Instruction } from '../client-pages/section-1-part-1'
-import { Question as Section1Part2Question } from '../client-pages/section-1-part-2'
 import { PureTestPartHeader as Header } from '../layouts/TestPartHeader'
 import { PureStandardTitle as StandardTitle } from '../components/StandardTitle'
 import { PureSubtitle as Subtitle } from '../components/Subtitle'
 import { PureReviewSectionHeader as SectionHeader } from '../components/ReviewSectionHeader'
 
+// Section Components
+import * as S1P1 from '../client-pages/section-1-part-1'
+
+// Contexts
 import SessionContext from '../contexts/SessionContext'
 
+// Services
 import * as Session from '../services/session'
 
+// Global CSS
 import '../components/App/App.css'
 
 const Root = styled.div`
@@ -64,15 +70,21 @@ function ReviewPage() {
                 </Header.Subtitle>
               </Header>
             </Review.Header>
+
             <Review.Section>
               <Review.Heading>
                 <SectionHeader>Section 1 Part 1</SectionHeader>
               </Review.Heading>
               <Review.Instruction>
-                <Section1Part1Instruction />
+                <S1P1.Instruction />
               </Review.Instruction>
+              <Review.AudioPlayer>
+                <S1P1.Audio />
+              </Review.AudioPlayer>
+              <Review.Response>
+                <S1P1.Question />
+              </Review.Response>
             </Review.Section>
-            <Section1Part1Question />
           </Review>
         </SessionContext.Provider>
       </Root>
